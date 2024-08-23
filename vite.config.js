25fa0b928afe84a913fb0f0e80d8208e1034f829
joshua <j.j.jatto@gmail.com>
2024-08-23 11:54:23 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/joshua-jatto/Emoji-Ecommerce-App.git'
+  base: '/Emoji-Ecommerce-App/'
 })
