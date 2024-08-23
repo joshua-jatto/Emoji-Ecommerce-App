11683b546cefd01740b7f55b9c8b4a0e2e11ce9b
joshua <j.j.jatto@gmail.com>
2024-08-23 19:49:55 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import NavBar from "./components/NavBar/NavBar";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home.jsx";
+import Home from "./pages/index.jsx";
 import Cart from "./pages/cart-page";
-import {ShopProvider} from "./context/shop-Context";
+import { ShopProvider } from "./context/shop-Context";
 
 export default function App() {
-
   return (
-    <div>
-      <ShopProvider>
-        <NavBar />
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route path="/cart-page" element={<Cart />}></Route>
-        </Routes>
-      </ShopProvider>
-    </div>
+    <ShopProvider>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/cart-page" element={<Cart />}></Route>
+      </Routes>
+    </ShopProvider>
   );
 }
