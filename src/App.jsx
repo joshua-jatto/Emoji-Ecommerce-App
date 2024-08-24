0aeb3ca2ac145682b30995c60874ff5cdc6bb97b
joshua <j.j.jatto@gmail.com>
2024-08-24 16:14:22 +0100

    fixed base path

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import NavBar from "./components/NavBar/NavBar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/index.jsx";
 import Cart from "./pages/cart-page";
-import { ShopProvider } from "./context/shop-Context";
 
 export default function App() {
   return (
-    <ShopProvider>
+    <div>
       <NavBar />
+
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/cart-page" element={<Cart />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart-page" element={<Cart />} />
       </Routes>
-    </ShopProvider>
+    </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles.css'
-import {BrowserRouter} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./styles.css";
+import { BrowserRouter } from "react-router-dom";
+import { ShopProvider } from "./context/shop-Context";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ShopProvider>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
-)
-
+    </BrowserRouter>
+  </ShopProvider>
+);
