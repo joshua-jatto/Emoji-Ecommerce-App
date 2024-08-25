ff7c5629e28efe41ec00c0e4d611a0413e650d44
joshua <j.j.jatto@gmail.com>
2024-08-25 22:19:36 +0100

    added commentary to fn

--- src/context/shop-Context.jsx
+++ src/context/shop-Context.jsx
@@ -6,6 +6,7 @@ const shopContext = createContext();
 export function ShopProvider({ children }) {
   const [cart, setCart] = useState(cartItems());
 
+  //initialize each object in PRODUCTS[] with an iniatial value of one.
   function cartItems(){
     let cart = {}
     for(let i =1; i < PRODUCTS.length+1 ; i++){
@@ -15,24 +16,30 @@ export function ShopProvider({ children }) {
     return cart
   }
 
+  //add +1 to the value of an object an its index
   function addToCart(productID) {
     setCart(prev => ({...prev, [productID]: prev[productID] +1 }));
   };
 
+
+  //Subtract -1 from the value of an object an its index
   function removeFromCart(productID){
     setCart(prev => ({...prev, [productID]: prev[productID] -1 }))
   };
 
+  //update value of an object an its index with an input value 
   function updateCartItemCount(quantity,productID){
     setCart(prev => ({...prev, [productID]: quantity }))
     
   };
 
+  //get total amount of items in cart
   function getTotalCartAmount(){
-    let totalAMount = 0;
+    let totalAMount = 0; // initial count
+    // loop for iterating through the array
     for(let i in cart){
-      console.log(i)
-      if(cart[i] > 0){
+
+      if(cart[i] > 0){ //if cart at i > 0 i.e [i] in cart
         let itemData = PRODUCTS.find(product => product.id === Number(i))
         totalAMount += cart[i] * itemData.price
       }
@@ -41,7 +48,6 @@ export function ShopProvider({ children }) {
     return totalAMount
   }
 
-  console.log(cart)
 
   const value = { addToCart, cart, removeFromCart, updateCartItemCount, getTotalCartAmount };
   return <shopContext.Provider value={value}>{children}</shopContext.Provider>;
